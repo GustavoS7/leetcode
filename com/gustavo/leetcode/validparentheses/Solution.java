2b5ab6bb4f15eabc91c1d4bed15212ca201039f6
GustavoS7 <gustavods2003@outlook.com>
2024-04-25 18:01:14 -0300

    valid parenthses complete, init mergetwosortedlist

--- com/gustavo/leetcode/validparentheses/Solution.java
+++ com/gustavo/leetcode/validparentheses/Solution.java
@@ -15,18 +15,17 @@ public class Solution {
 
     Stack<Character> stack = new Stack<Character>();
     
-    for (int i = 0; i < s.length(); i++) {
-      if (romamNumbers.containsKey(s.charAt(i))) {
-        stack.push(s.charAt(i));
+    for (char c : s.toCharArray()) {
+      if (romamNumbers.containsKey(c)) {
+        stack.push(c);
       } else {
-        if (!stack.empty() && romamNumbers.get(stack.lastElement()) == s.charAt(i)) {
+        if (!stack.empty() && romamNumbers.get(stack.lastElement()) == c) {
           stack.pop();
         } else return false;
       }
     }
 
-    if (stack.size() == 0) return true;
-    else return false;
+    return stack.isEmpty();
   }
 
   public static void main(String[] args) {
@@ -35,3 +34,40 @@ public class Solution {
     System.out.println(result);
   }
 }
+// package com.gustavo.leetcode.validparentheses;
+
+// import java.util.HashMap;
+// import java.util.Map;
+// import java.util.Stack;
+
+// public class Solution {
+//   public static boolean isValid(String s) {
+//     if (s.length() % 2 != 0 || s.length() == 0) return false;
+
+//     Map<Character, Character> romamNumbers = new HashMap<>();
+//     romamNumbers.put('[', ']');
+//     romamNumbers.put('{', '}');
+//     romamNumbers.put('(', ')');
+
+//     Stack<Character> stack = new Stack<Character>();
+    
+//     for (int i = 0; i < s.length(); i++) {
+//       if (romamNumbers.containsKey(s.charAt(i))) {
+//         stack.push(s.charAt(i));
+//       } else {
+//         if (!stack.empty() && romamNumbers.get(stack.lastElement()) == s.charAt(i)) {
+//           stack.pop();
+//         } else return false;
+//       }
+//     }
+
+//     if (stack.size() == 0) return true;
+//     else return false;
+//   }
+
+//   public static void main(String[] args) {
+//     Boolean result = isValid("(");
+
+//     System.out.println(result);
+//   }
+// }
