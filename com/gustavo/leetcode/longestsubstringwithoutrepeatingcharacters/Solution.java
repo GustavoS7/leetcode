6249e3cf77ea7eb7b245fa0ab3ed8805bdf56c70
GustavoS7 <gustavods2003@outlook.com>
2024-05-09 17:01:48 -0300

    dsadas

--- com/gustavo/leetcode/longestsubstringwithoutrepeatingcharacters/Solution.java
+++ com/gustavo/leetcode/longestsubstringwithoutrepeatingcharacters/Solution.java
@@ -1,9 +1,26 @@
 package com.gustavo.leetcode.longestsubstringwithoutrepeatingcharacters;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
-  public void lengthOfLongestSubstring(String s) {
-     CharSequence arr = s.chars();
+  public static int lengthOfLongestSubstring(String s) {
+    int max = 0;
 
+    Map<Character, Integer> list = new HashMap<Character, Integer>();
+    for (int i = 0, j = 0; i < s.length(); i++) {
+      if (list.containsKey(s.charAt(i))) {
+        j = Math.max(list.get(s.charAt(i)) + 1, j);
+      }
 
+      list.put(s.charAt(i), i);
+      max = Math.max(max, i - j + 1);
+    }
+
+    return max;
+  }
+
+  public static void main(String[] args) {
+    lengthOfLongestSubstring("sadsadsa");
   }
 }
