621390a8546a6d77016d28463029a65e5114edd4
GustavoS7 <gustavods2003@outlook.com>
2024-05-16 23:36:56 -0300

    dsadas

---
+++ com/gustavo/leetcode/longestsubstringwithoutrepeatingcharacter/Solution.java
@@ -0,0 +1,13 @@
+package com.gustavo.leetcode.longestsubstringwithoutrepeatingcharacter;
+
+public class Solution {
+  public int lengthOfLongestSubstring(String s) {
+    String arr = "";
+
+    for (int i = 0; i < s.length(); i++) {
+      if (arr.contains(s.charAt(i))) {
+        
+      }
+    }
+  }
+}
